b1490dc46db6bb123e01bacb9dc18096cb057bdf
Pratik Wadhai <81483293+pratik-wadhai@users.noreply.github.com>
2022-08-10 11:40:27 +0530

    Create life-in-weeks.js

---
+++ life-in-weeks.js
@@ -0,0 +1,20 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+ 
+var remainingYears = 90 - age;
+var days = remainingYears * 365;
+var weeks = remainingYears * 52;
+var months = remainingYears * 12;
+
+console.log("You have "+ days +" days, "+ weeks +" weeks, and "+ months +" months left.")
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+lifeInWeeks(51);
+
